b4328625bbe05bd9150941b815c9d0bed26a7aae
DarkAriesX <skrzys2103@gmail.com>
2022-11-01 02:55:57 +0100

    Added tripple_map class with basic funtionality (Still don't work)

--- tripple_map/tripple_map.hpp
+++ tripple_map/tripple_map.hpp
@@ -1,4 +1,27 @@
 #pragma once
 
 #include "../tripple/tripple.hpp"
+#include <initializer_list>
 
+namespace ks {
+template<typename _T1, typename _T2, typename _T3>
+class tripple_map {
+ public:
+    // Default constructor with key set to 1. Valid key value is in 1 - 3 range.
+    tripple_map(short key = 1);
+    // Constructor with array of tripple with key set to 1. Valid key value is in 1 - 3 range.
+    tripple_map(tripple<_T1, _T2, _T3> array[], short key = 1);
+
+    short getKey() { return key_; }
+ private:
+    short key_;
+};
+template<typename _T1, typename _T2, typename _T3>
+tripple_map<_T1, _T2, _T3>::tripple_map(short key) : key_(key) {
+
+}
+template<typename _T1, typename _T2, typename _T3>
+tripple_map<_T1, _T2, _T3>::tripple_map(tripple<_T1, _T2, _T3> array[], short key) : key_(key)  {
+    
+}
+}
